030653c95d27e62d6c5bb2415f229854025056a4
loshan2001 <loshansp2001@gmail.com>
2025-08-31 22:26:19 +0530

    learn about rest controller

---
+++ src/main/java/com/spring/springWebApp/HomeController.java
@@ -0,0 +1,12 @@
+package com.spring.springWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String greet(){
+        return "Welcome!";
+    }
+}
