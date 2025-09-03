1d978f68e1e6dcd889529a0d2e769ffe0047e428
loshan2001 <loshansp2001@gmail.com>
2025-09-03 18:37:04 +0530

    learnt about CRUD

--- src/main/java/com/spring/springWebApp/controller/ProductController.java
+++ src/main/java/com/spring/springWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.spring.springWebApp.controller;
 import com.spring.springWebApp.model.Product;
 import com.spring.springWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,28 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProduct(){
         return productService.getProducts();
+    }
+
+    @GetMapping("/products/{proId}")
+    public Product getProductById(@PathVariable int proId){
+        return productService.getProductById(proId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody  Product product){
+        productService.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product){
+        productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable("id") int proId){
+         productService.deleteProduct(proId);
     }
 }
--- src/main/java/com/spring/springWebApp/service/ProductService.java
+++ src/main/java/com/spring/springWebApp/service/ProductService.java
@@ -2,18 +2,55 @@ package com.spring.springWebApp.service;
 
 import com.spring.springWebApp.model.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101,"iphone",230000),
-            new Product(101,"cannon",700000));
+            new Product(102,"cannon",700000)));
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductById(int proId){
+        return  products.stream()
+                .filter(p -> p.getProId() == proId)
+                .findFirst().get();
+
+    }
+
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+
+    public void updateProduct(Product product) {
+        int index = 0;
+        for(int i=0; i<products.size(); i++){
+            if(products.get(i).getProId() == product.getProId()){
+                index = i;
+
+            }
+        }
+        products.set(index,product);
+    }
+
+    public void deleteProduct(int proId) {
+        int index = 0;
+        for(int i=0; i<products.size(); i++){
+            if(products.get(i).getProId() == proId){
+                index = i;
+                products.remove(index);
+            }
+        }
+
     }
 }
